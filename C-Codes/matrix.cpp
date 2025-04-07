e584abfbb933322b2835f0bb562546c52810b5dc
Шевцов Кирилл <shevtsov.km@phystech.edu>
2025-04-07 10:47:04 +0300

    Операции с матрицами, скоро добавлю вычисление детерминанта, понадобится для проекта

--- C-Codes/matrix.cpp
+++ C-Codes/matrix.cpp
@@ -1,110 +1,84 @@
+#pragma once
 #include <iostream>
-using std::cin;
-using std::cout;
-using std::endl;
+//Класс операций с матрицами
+class submatrix{
+    private:
+    //Указатель для двумерного динамического массива (матрицы)
+    double** matrix;
+    //Количество строк матрицы
+    int str;
+    //Количество столбцов матрицы
+    int cols;
 
-class matrix{
     public:
-    double data;
-
-    void print_arr(int string, int column){
-        if(string <= 0 || column <= 0){
+    //Выделение памяти под двумерный массив, создание матрицы заданного размера
+    submatrix(int m, int n): str(m), cols(n){
+        if((m <= 0) && (n <= 0)){
             throw std::invalid_argument("The matrix's parameters is only positive numbers!");
-            std::cout << std::endl;
         }
-        int **arr = new int*[string];
-        for(int it = 0; it < string; ++it){
-            arr[it] = new int[column];
-        }
-        for(int i = 0; i < string; ++i){
-            for(int j = 0; j < column; ++j){
-                std::cin >> arr[i][j];
-            }
-        }
-        for(int i = 0; i < string; ++i){
-            for(int j = 0; j < column; ++j){
-                std::cout << arr[i][j] << " ";
-            }
-            std::cout << std::endl;
+        matrix = new double*[m];
+        for(int i = 0; i < m; ++i){
+            matrix[i] = new double[n];
         }
     }
-
-    void summary(const int m, const int n){
-        int arr[m][n];
-        int brr[m][n];
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                std::cin >> arr[i][j];}}
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                std::cin >> brr[i][j];}}
-        int c[m][n];
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                c[i][j] = arr[i][j] + brr[i][j];}}
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                std::cout << c[i][j] << " ";
+    //Освобождение неиспользуемой памяти, удаление матрицы заданного размера
+    ~submatrix(){
+        for(int it = 0; it < str; ++it){
+            delete[] matrix[it];
+        }
+        delete[] matrix;
+    }
+    //Cложения двух матриц: если они разного размера, функция выдаёт ошибку
+    submatrix& operator+=(const submatrix& other){
+        if((str != other.str) && (cols != other.cols)){
+            throw std::invalid_argument("err");
+        }
+        for(int i = 0; i < str; ++i){
+            for(int j = 0; j < cols; ++j){
+                matrix[i][j] += other.matrix[i][j];
+            }
+        }
+        return *this;
+    }
+    //Вывод матрицы заданного размера на экран
+    void submatrix_print(){
+        for(int i = 0; i < str; ++i){
+            for(int j = 0; j < cols; ++j){
+                std::cout << matrix[i][j] << " ";
             }
             std::cout << std::endl;
+        } 
+    }
+    //Доступ к произвольному элементу матрицы: если вводится некорректный номер строки или столбца, функция выдаёт ошибку
+    double& submatrix_element(int i, int j){
+        return matrix[i][j];
+    }
+    //Произведение матриц, если их необходимые измерения не совпадают, то функция выдаёт ошибку
+    submatrix operator*(const submatrix& other) const {
+        if (cols != other.str) {
+            throw std::invalid_argument("The matrixes's sizes have incorrect form");
         }
-    } 
-    void matrix_multiply(const int m1, const int m2, const int n){
-        int arr[m1][n];
-        int brr[n][m2];
-        for(int i = 0; i < m1; ++i){
-            for(int j = 0; j < n; ++j){
-                std::cin >> arr[i][j];
-            }
-        }
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < m2; ++j){
-                std::cin >> brr[i][j];
-            }
-        }
-
-        int c[m1][m2] = {0};
-        for(int i = 0; i < m1; ++i){
-            for(int j = 0; j < m2; ++j){
-                c[i][j] = 0;
-                for(int k = 0; k < n; ++k){
-                    c[i][j] += arr[i][k] * brr[k][j];
+        submatrix result(str, other.cols);
+        for (int i = 0; i < str; ++i) {
+            for (int j = 0; j < other.cols; ++j) {
+                double sum = 0;
+                for (int k = 0; k < cols; ++k) {
+                    sum += matrix[i][k] * other.matrix[k][j];
                 }
-                std::cout << c[i][j] << " ";
+                result.matrix[i][j] = sum;
             }
-            std::cout << std::endl;
         }
+        return result;
     }
-    void transpose_matrix(int string, int column){
-        int **arr = new int*[string];
-        for(int it = 0; it < string; ++it){
-            arr[it] = new int[column];
-        }
-        for(int i = 0; i < string; ++i){
-            for(int j = 0; j < column; ++j){
-                std::cin >> arr[i][j];
+    //Транспонирование матрицы заданного размера
+    submatrix submatrix_transpose() const{
+        submatrix transpose(cols, str);
+        for(int i = 0; i < str; ++i){
+            for(int j = 0; j < cols; ++j){
+                transpose.matrix[j][i] = matrix[i][j];
             }
         }
-        int transpose[column][string];
-        for(int i = 0; i < string; ++i){
-            for(int j = 0; j < column; ++j){
-                transpose[j][i] = arr[i][j];
-            }
-        }
-        for(int i = 0; i < column; ++i){
-            for(int j = 0; j < string; ++j){
-                std::cout << transpose[i][j] << " ";
-            }
-            std::cout << std::endl;
-        }
+        return transpose;
     }
 };
-int main(){
-    matrix mat;
-    mat.print_arr(2, 2);
-    mat.summary(2, 2);
-    mat.matrix_multiply(2, 2, 2);
-    mat.transpose_matrix(3, 3);
 
-
-}
