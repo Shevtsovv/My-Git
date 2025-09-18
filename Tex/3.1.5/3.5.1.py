883b77faf830b47d0c141f38f0159f9cc30be6dc
Шевцов Кирилл <shevtsov.km@phystech.edu>
2025-09-18 16:55:43 +0300

    изменил графики

--- Tex/3.1.5/3.5.1.py
+++ Tex/3.1.5/3.5.1.py
@@ -22,3 +22,22 @@ plt.minorticks_on()
 plt.legend()
 plt.title('Вольт-амперная характеристика газового разряда')
 plt.savefig('vax.png')
+import matplotlib.pyplot as plt
+import numpy as np
+data = np.loadtxt('txt/zond.txt')
+markers = ['o', 's', '^', 'D']
+amperas = ['$5.000\pm 0.001$', '$4.005\pm 0.001$', '$3.101\pm 0.001$', '$1.513\pm 0.001$']
+plt.figure(figsize=(10, 6))
+for i in range(0, data.shape[1], 2):
+    if i + 1 < data.shape[1]:
+        x = data[:, i]
+        y = data[:, i+1]
+        plt.plot(x, y, label=f'Ток разряда {amperas[i//2]}', marker=markers[i//2], linewidth=1.2, markersize=5)
+plt.xlabel('Напряжение, В')
+plt.ylabel('Сила тока, А')
+plt.title('Зондовые характеристики при разных токах разряда')
+plt.legend()
+plt.grid(True, which='major', linewidth=1.2)
+plt.grid(True, which='minor', linewidth=1.2)
+plt.minorticks_on()
+plt.savefig('zond.png')
